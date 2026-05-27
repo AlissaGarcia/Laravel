d66c73d82764f7d4d9afa44ae4261452e4bcc4e0
Alissa Garcia <118315759+AlissaGarcia@users.noreply.github.com>
2026-05-27 01:26:51 +0000

    Finalização, produtos rotas views e comparação de funcionamento com create

--- Atividade05/routes/web.php
+++ Atividade05/routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CursoController;
 use App\Http\Controllers\AlunoController;
+use App\Http\Controllers\DisciplinaController;
+use App\Http\Controllers\ProdutoController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -21,3 +23,15 @@ Route::get('/cursos/formulario', [CursoController::class, 'formulario']);
 Route::post('/cursos/store', [CursoController::class, 'store']);
 
 Route::resource('alunos', AlunoController::class);
+
+Route::get('/disciplinas', [DisciplinaController::class, 'index']);
+
+Route::get('/disciplinas/create', [DisciplinaController::class, 'create']);
+
+Route::post('/disciplinas/store', [DisciplinaController::class, 'store']);
+
+Route::get('/disciplinas/{id}', [DisciplinaController::class, 'show']);
+
+Route::get('/produtos/create', [ProdutoController::class, 'create']);
+
+Route::post('/produtos', [ProdutoController::class, 'store']);
